a57e81bbbde05f25dab97feddb1d13b739d0c3a9
Ryan Sy <ryansy@Ryans-Air.fios-router.home>
2022-02-01 23:14:41 -0500

    created services

---
+++ src/main/java/com/revature/routes/Route.java
@@ -0,0 +1,16 @@
+package com.revature.routes;
+
+import io.javalin.Javalin;
+
+public abstract class Route {
+
+    public static void establishRoutes(Javalin app, Route ... routes){
+        for(int i=0; i<routes.length; i++){
+            Route r = routes[i];
+            r.registerLocalRoutes(app);
+        }
+    }
+
+    abstract public void registerLocalRoutes(Javalin app);
+
+}
